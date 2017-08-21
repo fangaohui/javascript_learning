9b882999277c84598a1fbdedfeea334ee9c185da
MichaelFan <fan122316@gmail.com>
2017-08-22 07:32:21 +0800

    4.1

--- professional_javascript_for_web_developers/learning.js
+++ professional_javascript_for_web_developers/learning.js
@@ -12,7 +12,7 @@ function doSomething() {
     alert(messageFirst);  //undefined
     alert(messageSecond);  //messageSecond并没有声明 是未定义的变量 产生错误
     alert(typeof message);  //typeof是操作符而不是函数 括号不是必须的 undefined
-    alert(typeof (messageSecond));  //未声明的变量只能执行一项操作 typeof 结果也是undefined
+    alert(typeof (messageSecond));  //未声明的变量只能执行一项操作typeof 结果也是undefined
 
     //Null类型只有一个值null 表示一个空对象引用
     var car = null;
@@ -21,6 +21,7 @@ function doSomething() {
     if (car != null) {
     }
     alert(null == undefined);  //true 但由于类型不一致 会出于比较的目的转换操作数
+    alert(null === undefined);  //false 全等不转换操作数类型
 
     //ECMAScript中所有类型的值都有与bool值等价的值 用函数Boolean()将一个其他类型的值转换为其对于的bool值
     var messageThird = "hello";
@@ -38,11 +39,13 @@ function doSomething() {
     var floatNum1 = 10.0;  //其实为整数 解析为10
     var floatNum2 = 0.0000003;  //小数点6个0以上默认转换为e表示法 3e-7
     var floatNum3 = 0.1, floatNum4 = 0.2;
-    //0.1+0.2实际等于0.30000000000000004 以下判断为false
+    //0.1+0.2实际等于0.30000000000000004 以下判断为false IEEE754通病
     if (floatNum3 + floatNum4 == 0.3) {
     }
-    //依次为 极小极大值 负正无穷大 无穷大不是能够参与计算的数值
-    var minValue = Number.MIN_VALUE, maxValue = Number.MAX_VALUE, maxInfinityValue = Number.NEGATIVE_INFINITY, minInfinityValue = POSITIVE_INFINITY;
+    //依次为 极小极大值
+    var minValue = Number.MIN_VALUE, maxValue = Number.MAX_VALUE;
+    //负 正 正无穷大 无穷大不是能够参与计算的数值
+    var maxInfinityValue = Number.NEGATIVE_INFINITY, minInfinityValue = POSITIVE_INFINITY ,minInfinityValue1 = Infinity;
     var testResult = maxValue + maxValue;
     alert(isFinite(testResult));  //false 使用isFinite()函数判断值是否为极大极小之间的正常值
 
@@ -122,10 +125,59 @@ function doSomething() {
     //逻辑与操作符和或操作符 为短路操作符 可能返回一个对象 注意对于规则 因此可以利用逻辑或操作符来避免为变量赋值null或undefined
     var myObject = preferredObject || backupObject;  //preferredObject优先值 如优先值为null则使用后备值backupObject
 
+    //ECMAScript中不存在块级作用域
+    var count = 10;
+    for (var i = 0; i < count; i++) {
+        alert(i);
+    }
+    //循环内部定义的变量在外部也可以访问
+    alert(i);
 
+    //label语句 outermost 结合break continue在循环嵌套的情况下使用
+    var num = 10;
+    outermost:
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if (i == 5 && j == 5) {
+                coninue outermost;
+            }
+            num++;
+        }
+    }
+    alert(num);
 
+    //switch语句比较值使用的是全等操作符 不会发生类型转换
+    //switch语句中可以使用任何数据类型 每个case值可以是变量或表达式
+    var num = 25;
+    switch (true) {
+        case num < 0: {
+            break;
+        }
+        case num >= 0 && num <= 10: {
+            break;
+        }
+        case num > 10 && num <= 20: {
+            break;
+        }
+        default: {
+        }
+    }
 }
 
+//ECMAScript中的参数在内部使用一个数组来表示的 通过arguments对象访问参数数组但arguments本身不是数组
+//函数没有签名 故没有重载 命名的参数只提供便利 解析器不会验证命名参数
+function sayHi() {
+    alert('first:' + arguments[0] + ',second:' + arguments[1] + ',total:' + arguments.length);
+}
+//arguments[1]值会同步给num2 但两个值内存空间是独立的 num2是否会同步给rguments[1]？
+function doAdd(num1, num2) {
+    sayHi(1, 2, 3, 4);
+    arguments[1] = arguments.length;
+    alert(arguments[0] + num2);
+}
+//ECMAScript中的所有参数传递的都是值 不可能通过引用传递参数？ 对象如何通过参数传递？
+//如果只传一个参数 设置arguments[1]不会反映到命名参数中 因为arguments对象的长度是由传入参数的个数决定 不是由定义函数时的命名参数个数决定
+doAdd(1);
 
 
 
