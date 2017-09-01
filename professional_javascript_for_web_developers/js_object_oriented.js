49d08ff86a80d70887fd09ff5c5421690d9fa8e9
MichaelFan <fan122316@gmail.com>
2017-09-01 08:06:43 +0800

    6.2.3

--- professional_javascript_for_web_developers/js_object_oriented.js
+++ professional_javascript_for_web_developers/js_object_oriented.js
@@ -77,7 +77,7 @@ var result = test1 + '1';
 console.log(result); //undefined1
 result = test1 + 1; //NAN
 
-//6.2
+//6.2 创建对象
 function Person(name, age){
     this.name = name;
     this.age = age;
@@ -95,6 +95,101 @@ person1.sayName();
 var o = new Object();
 Person.call(o, 'kity', 25);
 o.sayName(); //kity 使用函数的call()函数
+
+//6.2.3 原型模式
+function Student(){}
+Student.prototype.name = 'Ashliy';
+Student.prototype.age = 3;
+Student.prototype.sayName = function(){
+    console.log(this.name);
+    console.log(this); //Student {} prototype原型对象
+};
+var student1 = new Student();
+student1.sayName(); //Ashliy
+var student2 = new Student();
+student2.sayName(); //Ashliy
+console.log(student1.sayName === student2.sayName); //true
+console.log(student1.__proto__ === Student.prototype); //true 调用构造函数创建新实例后 包含一个指向原型对象的指针 内部属性[[Prototype]] 使用__proto__访问
+console.log(student2.__proto__ === Student.prototype); //true 调用构造函数创建新实例后 包含一个指向原型对象的指针 内部属性[[Prototype]] 使用__proto__访问
+console.log(student1.__proto__ === student2.__proto__); //true 调用构造函数创建新实例后 包含一个指向原型对象的指针 内部属性[[Prototype]] 使用__proto__访问
+console.log(student1.constructor === student2.constructor); //true
+console.log(student1.constructor === Student.prototype.constructor); //true
+console.log(Student.prototype.isPrototypeOf(student1)); //true
+console.log(Student.prototype.isPrototypeOf(person1)); //false
+person2.customPrototype = student1.__proto__;
+console.log(Student.prototype.isPrototypeOf(person2)); //false 必须是对象的内部属性__proto__
+console.log('****分割线****');
+console.log(Object.getPrototypeOf(student1) === Student.prototype);
+student2.name = 'Answer';
+student2.sayName = function(){
+    console.log('Michael' + this.name);
+};
+student1.sayName(); //Michael 来至实例
+student2.sayName(); //Ashliy 来至原型
+student2.__proto__.sayName(); //Ashliy
+student2.sayName = undefined;
+console.log('****分割线****');
+console.log(student2.sayName); //undefined 只能使用delete操作符才能完全删除实例属性
+Object.defineProperty(Student.prototype, 'name', {
+    configurable : false,
+    enumerable : false //下面的for in中不会出现name属性
+});
+Object.defineProperty(student2, 'name', {
+    configurable : false,
+    enumerable : false //下面的for in中不会出现name属性
+});
+Object.defineProperty(student1, 'name', {
+    enumerable : true, //下面的for in中会出现name属性 尽管prototype为false
+    value : '123'
+});
+delete student2.sayName; //只能使用delete操作符才能完全删除实例属性 从而重新访问原型中属性
+delete student2.name; //无效 因为上面设置configurable为false
+student2.sayName(); //Answer
+for (var prop in student2) {
+    console.log('*********');
+    console.log(prop);
+}
+for (var prop in student1) {
+    console.log('~~~~~~~~~');
+    console.log(prop);
+    if (prop == 'name') {
+        console.log(student1.name); //123
+    }
+}
+var allKeys = Object.keys(student1);
+console.log(allKeys); //[ 'name' ] 只能取得对象中的可枚举属性 不包含原型中的
+allKeys = Object.getOwnPropertyNames(Student.prototype);
+console.log(allKeys); //[ 'constructor', 'name', 'age', 'sayName' ] 和keys()的区别是 包括不可枚举的属性
+//~~~
+function test(){};
+console.log(test.prototype); //test {} 原型对象
+console.log(test.prototype.constructor); //[Function: test] prototype所在函数指针
+//~~~
+//对constructor的理解 student1.constructor和Student.prototype.constructor的关系???
+console.log(student1.constructor === Student.prototype.constructor); //true
+function Teacher(){};
+Teacher.prototype = {
+    name : 'Michael',
+    age : 29,
+    sayName : function(){
+        console.log(this.name);
+    }
+};
+var ling = new Teacher();
+console.log(ling instanceof Teacher); //true
+console.log(ling.constructor == Object); //true
+console.log(ling.constructor == Teacher); //false
+Object.defineProperty(Teacher.prototype, 'constructor', {
+    enumerable : false,
+    value : Teacher
+});
+console.log(ling.constructor == Teacher); //true
+
+
+
+
+
+
 
 
 
