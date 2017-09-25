d09082b7155ff917a25be4437b52ce8a13ede9f1
MichaelFan <fan122316@gmail.com>
2017-09-26 07:32:11 +0800

    ES6 part9

--- professional_javascript_for_web_developers/ECMAScript 6/part8.js
+++ professional_javascript_for_web_developers/ECMAScript 6/part8.js
@@ -199,4 +199,63 @@ var abc = {
 abc.testDelegate(123,function(){
     console.log(this);
 });
+function run(taskDef){
+    let task = taskDef();
+    let result = task.next();
+    function step(){
+        if (!result.done) {
+            result = task.next(result.value+100);
+            console.log(result);
+            step();
+        }
+    };
+    step();
+}
+run(function*(){
+    let value = yield 1;
+    console.log(value);
+
+    value = yield value + 3;
+    console.log(value);
+});
+console.log('异步任务执行器');
+function fetchData(){
+    return function(callback){
+        callback(null,'hi');
+    };
+}
+var shit = fetchData();
+shit(function(ac,bc){
+    console.log(ac);
+    console.log(bc);
+});
+function asyncRun(taskDef){
+    let task = taskDef();
+    let result = task.next();
+    function step(){
+        if (!result.done) {
+            if (typeof result.value === 'function') {
+                result.value(function(err,data){
+                    if (err) {
+                        result = task.throw(err);
+                        return;
+                    }
+                    result = task.next(data);
+                    step();
+                });
+            } else {
+                result = task.next(result.value);
+                step();
+            }
+        }
+    };
+    step();
+};
+asyncRun(function*(){
+    let contents = yield fetchData(123);
+    console.log('Done with ' + contents);
+});
+
+
+
 
