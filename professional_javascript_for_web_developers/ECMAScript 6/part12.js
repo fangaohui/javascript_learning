ce602e6c84e78e583fe316aaf203c2fc2863a2d9
MichaelFan <fan122316@gmail.com>
2017-10-13 07:32:00 +0800

    ES6 p309

--- professional_javascript_for_web_developers/ECMAScript 6/part12.js
+++ professional_javascript_for_web_developers/ECMAScript 6/part12.js
@@ -94,5 +94,98 @@ let descriptorTest = {
 };
 // Object.defineProperty(proxy1,Symbol('asd'),descriptorTest); 为什么报错???
 console.log('覆写抽象基类构造函数');
+class AbstractNumbers{
+    constructor(...values){
+        if (new.target === AbstractNumbers) {
+            throw new TypeError('抽象基类');
+        }
+        this.values12 = values;
+    }
+};
+class Numbers extends AbstractNumbers{
+    constructor(name){
+        super(1,2);
+        this.name = name;
+    }
+};
+let AbstractNumbersProxy = new Proxy(AbstractNumbers,{
+    construct : function(trapTarget,argumentlist){
+        //通过指定一个new.target为function 绕过抽象基类构造函数的判断
+        return Reflect.construct(trapTarget,argumentlist,function(){});
+    }
+});
+console.log(typeof AbstractNumbersProxy); //function
+// instance11 = new AbstractNumbers(1,2,3); //error
+let instance11 = new AbstractNumbersProxy(1,2,3);
+console.log(instance11.values12); //[ 1, 2, 3 ]
+let NumbsersProxy,revoke;
+//注意该形式解构必须用()包裹
+({proxy : NumbsersProxy,revoke} = Proxy.revocable(Numbers,{
+    apply : function(trapTarget,thisArg,argumentlist){
+        return new trapTarget(...argumentlist);
+    }
+}));
+let testNumbers = NumbsersProxy('M');
+//需要考虑两个问题 1通过代理调用Numbers构造函数 未影响到其内部super() 2同样是代理和目标的实例 instanceof通过原型链来确定此信息 而原型链查找不受代理影响
+//需要复习instanceof和typeof内部实现
+console.log(testNumbers.name); //M
+console.log(testNumbers.values12); //[ 1, 2 ]
+console.log(testNumbers instanceof NumbsersProxy); //true
+console.log(testNumbers instanceof Numbers); //true
+console.log(testNumbers instanceof AbstractNumbersProxy); //true
+console.log(testNumbers instanceof AbstractNumbers); //true
+revoke();
+// testNumbers = NumbsersProxy('n'); //error Proxy.revocable创建的可撤销代理已执行过revoke 已被撤销
+console.log('解决数组问题');
+function toUint32(value){
+    return Math.floor(Math.abs(Number(value))) % Math.pow(2,32);
+}
+function isArrayIndex(key){
+    let numericKey = toUint32(key);
+    return String(numericKey) == key && numericKey < (Math.pow(2,32) - 1);
+}
+class MyArray{
+    constructor(length = 0){
+        this.length = length;
+        return new Proxy(this,{
+            set(trapTarget,key,value){
+                let currentLength = Reflect.get(trapTarget,'length');
+                if (isArrayIndex(key)) {
+                    let numericKey = Number(key);
+                    if (numericKey > currentLength) {
+                        Reflect.set(trapTarget,'length',numericKey+1);
+                    }
+                } else if (key === 'length'){
+                    if (value < currentLength) {
+                        for (let i = currentLength - 1; i >= value; i--) {
+                            Reflect.deleteProperty(trapTarget,i);
+                        }
+                    }
+                }
+                Reflect.set(trapTarget,key,value);
+            }
+        });
+    }
+}
+let colors = new MyArray(3);
+console.log(colors instanceof MyArray); //true
+console.log(colors.length); //3
+colors[1] = 234;
+colors[6] = 123;
+console.log(colors.length); //7
+console.log(colors); //MyArray { '1': 234, '6': 123, length: 7 }
+colors.length = 2;
+console.log(colors); //MyArray { '1': 234, length: 2 }
+console.log('将代理用作原型');
+let target23 = {};
+let thing = Object.create(new Proxy(target23,{
+    get(trapTarget,key,receiver){
+        console.log(trapTarget === receiver); //false 注意不相等 trapTarget是receiver的原型对象
+        throw new ReferenceError(`${key} doesn't exist`);
+    }
+}));
+thing.name = 'thing';
+console.log(thing.name); //thing
+// var unk = thing.unk; //error
 
 
