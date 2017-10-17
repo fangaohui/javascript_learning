cd80cb38485d6c4c0a5ec804fd6bfe9d8265cf7e
MichaelFan <fan122316@gmail.com>
2017-10-18 07:17:07 +0800

    review

--- professional_javascript_for_web_developers/ES6_review.js
+++ professional_javascript_for_web_developers/ES6_review.js
@@ -4,3 +4,13 @@ function getValue(){
 //初次解析函数声明不会调用getValue()方法 方法执行时且使用默认参数才执行
 function add(a,b = getValue()){
 }
+const setCookieDefaults = {
+    secure : false,
+    path : '/',
+    domain : 'hupu.com'
+};
+function setCookie(name,value,{
+    secure = setCookieDefaults.secure,
+    path = setCookieDefaults.path,
+    domain = setCookieDefaults.domain
+} = setCookieDefaults);
