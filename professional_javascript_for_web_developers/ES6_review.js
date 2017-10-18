d9048dc79dfbc4d6069c1f3e9c912bfb80489f3a
MichaelFan <fan122316@gmail.com>
2017-10-19 07:31:33 +0800

    review

--- professional_javascript_for_web_developers/ES6_review.js
+++ professional_javascript_for_web_developers/ES6_review.js
@@ -13,4 +13,46 @@ function setCookie(name,value,{
     secure = setCookieDefaults.secure,
     path = setCookieDefaults.path,
     domain = setCookieDefaults.domain
-} = setCookieDefaults);
+} = setCookieDefaults){
+}
+//普通变量和对象属性之间的区别和联系
+var test = {
+    have : 123,
+    testF(){
+        // console.log(have); //error have未定义
+        return 345;
+    }
+};
+test.testF();
+function *test1(){
+    yield 2;
+    var s = yield test.testF();
+    yield s;
+}
+var y = test1();
+console.log(y.next());
+console.log(y.next());
+console.log(y.next()); //{ value: undefined, done: false } yield后函数执行的return值只对生成器函数有用
+var aaa = function(){
+    console.log(aaa); //[Function: aaa]
+}
+aaa();
+var bbb = class {
+    constructor(name){
+        this.name = name;
+        console.log(bbb);
+    }
+    ccc(){
+        console.log(123321);
+        console.log(bbb);
+    }
+};
+console.log(bbb.__proto__); //[Function]
+var abc = new bbb();
+abc.ccc();
+
+
+
+
+
+
