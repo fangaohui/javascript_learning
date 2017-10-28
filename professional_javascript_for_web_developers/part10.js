772e6d60fabd8961662dee63f02deab1afdf3d6e
MichaelFan <fan122316@gmail.com>
2017-10-28 18:23:41 +0800

    part 11

--- professional_javascript_for_web_developers/part10.js
+++ professional_javascript_for_web_developers/part10.js
@@ -1,3 +1,4 @@
+/*
 alert(Node.ELEMENT_NODE); //1
 alert('test\n' + document.childNodes.length + '\n' + document.childNodes.item(0) + '\n' + (document.childNodes[0] === document.childNodes.item(0)) + '\n' + document.childNodes.item(1) + '\n' + document.childNodes.item(2));
 let arrayOfNodes = Array.prototype.slice.call(document.childNodes,0);
@@ -31,4 +32,51 @@ function outputAttributes(element) {
 }
 alert(as[0].attributes);
 alert(outputAttributes(as[0]));
+*/
+let testElement = document.getElementsByTagName('a');
+testElement = document.getElementById('abc');
+testElement.testClone = 'a';
+alert(testElement.testClone);
+var cloneE = testElement.cloneNode(true);
+alert(cloneE.testClone); //undefined cloneNode即便是深复制 也只是复制节点特性和子节点 不会复制节点对象属性 注意特性和属性的区别
+// document.domain = 'www.hupu.com'; //error 当前加载本地文件domian是''空字符串 设置新domain只能是旧domian的父域
+// window.onload = function(){
+//     document.write('hello');
+// };
+alert(document.documentElement.tagName); //HTML注意tagName在HTML中返回的都是大写
+var textEle = testElement.childNodes[0];
+alert(textEle.data);
+textEle.appendData('some <strong>');
+textEle.nodeValue = 'any <a12>';
+alert(document.doctype.name);
+//DocumentFragment类型 可用来避免逐个添加元素导致浏览器反复渲染 可先加入到DocumentFragment类型然后一次性添加到文档中
+var fragment = document.createDocumentFragment();
+var testP = null;
+for (var i = 3; i >= 0; i--) {
+    testP = document.createElement('p');
+    testP.appendChild(document.createTextNode('item' + i));
+    fragment.appendChild(testP);
+}
+testElement.appendChild(fragment);
+var divs = document.getElementsByTagName('a'),
+    i,
+    len,
+    div;
+for (i = 0,len = divs.length; i < len; i++) {
+    div = document.createElement('a');
+    div.appendChild(document.createTextNode(`${i}`));
+    document.body.appendChild(div);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
