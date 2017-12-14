a934b73b125a1426e6b715bcc4fb7fd2b0bfad10
MichaelFan <fan122316@gmail.com>
2017-12-15 07:25:23 +0800

    21.1.1

--- professional_javascript_for_web_developers/part20.js
+++ professional_javascript_for_web_developers/part20.js
@@ -1,9 +1,32 @@
-var json = '[{"title":123,"subtitle":234},{"title":123,"subtitle":234}]';
+var json = '[{"title1":123,"subtitle":234,"title":{"title":555,"aaa":123}},{"title":123,"subtitle":234}]';
 var jsonObj = eval(json);
 var jsonObj1 = JSON.parse(json);
 alert(jsonObj1[0]['title']);
-jsonObj.toJSON = function () {
-    return 'test to json';
-}
-var jsonStr = JSON.stringify(jsonObj);
+// jsonObj.toJSON = function () {
+//     return 'test to json';
+// }
+var jsonStr = JSON.stringify(jsonObj,['title']);
 alert(jsonStr);
+
+function createXHR(){
+    if (typeof XMLHttpRequest != 'undefined') {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != 'undefined') {
+        if (typeof arguments.callee.activeXString != 'string') {
+            var versions = ['...','...'],
+            i,len;
+            for (var i = 0,len = versions.length; i < len; i++) {
+                try{
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    //跳过
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error('no xhr object');
+    }
+}
