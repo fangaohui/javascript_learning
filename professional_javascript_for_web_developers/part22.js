459e2f9ad96f17ccf0555dcbf7f5f546fc876776
MichaelFan <fan122316@gmail.com>
2017-12-21 07:29:59 +0800

    22.3

--- professional_javascript_for_web_developers/part22.js
+++ professional_javascript_for_web_developers/part22.js
@@ -25,7 +25,19 @@ var sub = new SubPerson(12);
 console.log(sub.name);
 
 function bind(fn,context){
+    var args = Array.prototype.slice.call(arguments,2);
     return function(){
-        return fn.apply(context,arguments);
+        var innerArgs = Array.prototype.slice.call(arguments);
+        var finalArgs = args.concat(innerArgs);
+        return fn.apply(context,finalArgs);
     };
 }
+//不可扩展对象
+Object.preventExtensions(sub);
+Object.isExtensible(sub);
+//密封的对象 属性的[[Configurable]]特性都被设置为false 不能删除属性和方法 part6
+Object.seal(sub);
+Object.isSealed(sub);
+//冻结的对象 在密封的基础上 属性的[[Writable]]特性都被设置为false
+Object.freeze(sub);
+Object.isFrozen(sub);
